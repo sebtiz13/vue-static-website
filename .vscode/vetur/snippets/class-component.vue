454aa2624928182cd5086abde551949ebbf50675
Sébastien Rodriguez <sebtiz13@gmail.com>
2020-05-09 01:29:32 +0200

    chore: add snippet for component with Typescript

---
+++ .vscode/vetur/snippets/class-component.vue
@@ -0,0 +1,12 @@
+<template>
+  ${0}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MyComponent extends Vue {
+
+}
+</script>
