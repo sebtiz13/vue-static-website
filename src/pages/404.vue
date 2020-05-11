cb2eff3aadda9df0660013c340932b49564173a3
Sébastien Rodriguez <sebtiz13@gmail.com>
2020-05-11 22:06:46 +0200

    feat(layout): add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <template slot="header">
+      <g-image
+        alt="Example image"
+        src="~/assets/headers/not_found-header.png"
+      />
+    </template>
+    <h2 class="content-title main-title">
+      Hey are you lost ?
+    </h2>
+  </Layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { MetaInfo } from 'vue-meta';
+
+@Component
+export default class ErrorPage extends Vue {
+  metaInfo(): MetaInfo {
+    return {
+      title: 'Page not found',
+    };
+  }
+}
+</script>
